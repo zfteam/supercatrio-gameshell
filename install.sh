7394ec3f8e81dce77f7386e14f2bfe016cf1caa4
cjv123 <cjv123@qq.com>
2019-08-29 15:41:59 +0800

    lazy install

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 sudo apt-get install -y libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev
-mv 01_SuperCatrio ~/apps/Menu/21_Indie\ Games/
+git clone https://github.com/zfteam/supercatrio-gameshell.git
+mv supercatrio-gameshell/01_SuperCatrio ~/apps/Menu/21_Indie\ Games/
 chmod 766 ~/apps/Menu/21_Indie\ Games/01_SuperCatrio/supercatrio
 chmod 766 ~/apps/Menu/21_Indie\ Games/01_SuperCatrio/SuperCatrio.sh
+rm -rf supercatrio-gameshell
 echo "install complete! please refresh launcher!"
